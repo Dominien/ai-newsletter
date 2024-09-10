24503fe0faf2467afbb72c734762c6b557d56b4a
Marco <marcopatzelt7@gmail.com>
2024-09-10 08:05:54 +0200

    Updated homepage content to German and customized the blog layout

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,9 +1,8 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
+  { href: '/tags', title: 'Themen' },
+  { href: '/about', title: 'Autor' },
 ]
 
 export default headerNavLinks
