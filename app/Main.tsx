3acdf23fcff1808368edd64e2a083a2aa907493f
Marco <marcopatzelt7@gmail.com>
2024-09-11 07:45:20 +0200

    Updated homepage content to German and customized the blog layout

--- app/Main.tsx
+++ app/Main.tsx
@@ -51,7 +51,10 @@ export default function Home({ posts }) {
                         </div>
                         <div>
                           <h2 className="text-2xl font-bold leading-8 tracking-tight">
-                            <Link href={`/blog/${slug}`} className="text-gray-900 dark:text-gray-100">
+                            <Link
+                              href={`/blog/${slug}`}
+                              className="text-gray-900 dark:text-gray-100"
+                            >
                               {title}
                             </Link>
                           </h2>
