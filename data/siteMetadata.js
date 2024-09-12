ad0db6b696b4c09f642dda0cb4cb24dbc2882e3c
Marco <marcopatzelt7@gmail.com>
2024-09-12 10:21:14 +0200

    Updated homepage content to German and customized the blog layout

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,6 +1,6 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'KI-Agenda – Ihre Quelle für Künstliche Intelligenz Nachrichten',
+  title: 'KI-Agenda',
   author: 'Marco Patzelt',
   headerTitle: 'KI-Agenda',
   description:
